0c53192569879075656dc8d4f31d65fe3c647c03
bazardosoneka-stack <bazardosoneka@gmail.com>
2026-01-30 17:03:52 -0300

    Create l.js

---
+++ l.js
@@ -0,0 +1,6 @@
+document.getElementById('f').onsubmit=e=>{
+  e.preventDefault();
+  if(document.getElementById('u').value==='rlk'
+  && document.getElementById('p').value==='1337')
+     location='d.html';
+};
