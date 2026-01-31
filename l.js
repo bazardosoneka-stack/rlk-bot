1cbf223b787972fb322f22887274c6aa53e06cbf
bazardosoneka-stack <bazardosoneka@gmail.com>
2026-01-31 13:14:21 -0300

    Delete l.js

--- l.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('f').onsubmit=e=>{
-  e.preventDefault();
-  if(document.getElementById('u').value==='rlk'
-  && document.getElementById('p').value==='1337')
-     location='d.html';
-};
